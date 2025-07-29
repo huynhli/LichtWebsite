b6a460fededd50ad84d3a58f3c3c3ee4afb33707
TrachScor6 <liam.huynh@mail.utoronto.ca>
2025-07-29 09:24:13 -0400

    testing with framer
    temp push

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,25 +1,56 @@
-export default function Header(){
-	const scrollToSpot = (pixels: number): void => {
-		window.scrollTo({ top: pixels, behavior: 'smooth'});
+import { motion, useAnimation } from "framer-motion";
+import { useState } from "react";
+
+type HeaderProps = {
+	onScrollToAbout: () => void
+	onScrollToNews: () => void
+}
+
+export default function Header({ onScrollToAbout, onScrollToNews }: HeaderProps){
+	const controls = useAnimation()
+	const [navClicked, setNavClicked] = useState<boolean>(false)
+
+	const expandNav = async () => {
+		if (!navClicked){
+			await controls.start({
+				rotate: 90,
+				transition: { duration: 0.7, ease: "easeInOut"},
+			})
+			// expand nav bar
+		} else {
+			await controls.start({
+				rotate: 0,
+				transition: { duration: 0.7, ease: "easeInOut"},
+			})
+			// close nav bar
+		}
+		setNavClicked(prev => !prev)
+		
 	}
 
 	return (
 		<div className={`flex flex-row h-12 px-2 mt-[5%] bg-red-300 rounded-lg w-full md:w-auto md:mx-[10%]`}>
 				
-				<div className="hidden md:flex flex-1 justify-center items-center bg-green-400"><a onClick={() => scrollToSpot(700)} className={`inline-block mx-2 cursor-pointer font-bold text-[clamp(2rem,3vw,5rem)] transition-all duration-300 hover:scale-125 `}>About</a></div>
+				<div className="hidden md:flex flex-1 justify-center items-center bg-green-400"><a onClick={onScrollToAbout} className={`inline-block mx-2 cursor-pointer font-bold text-[clamp(2rem,3vw,5rem)] transition-all duration-300 hover:scale-125 `}>About</a></div>
 				<div className="flex-1 flex items-center justify-between md:justify-center bg-green-200 md:flex-col">
 					<a
-						onClick={() => scrollToSpot(950)}
+						onClick={onScrollToAbout}
 						className="inline-block mx-2 cursor-pointer font-bold text-lg transition-all duration-300 hover:scale-125 max-w-[30%] md:max-w-[80%]"
 					>
 						<img src="/licht_logo.png" />
 					</a>
-					<a className="flex md:hidden ml-2 w-10 mr-[5%] bg-blue-400">
-						<img src="/hamburg.png" />
+					<a className="flex md:hidden ml-2 w-10 mr-[5%] cursor-pointer">
+						<motion.img
+							src="/hamburg.png"
+							onClick={expandNav}
+							className="flex invert md:hidden ml-2 w-10 mr-[5%] cursor-pointer"
+							animate={controls}
+							initial={{ rotate: 0 }}
+						/>
 					</a>
 				</div>
 
-				<div className="hidden md:flex flex-1 justify-center items-center bg-green-400"><a onClick={() => scrollToSpot(950)} className={`inline-block mx-2 cursor-pointer font-bold text-[clamp(2rem,3vw,5rem)] transition-all duration-300 hover:scale-125`}>News</a></div>
-			</div>
+				<div className="hidden md:flex flex-1 justify-center items-center bg-green-400"><a onClick={onScrollToNews} className={`inline-block mx-2 cursor-pointer font-bold text-[clamp(2rem,3vw,5rem)] transition-all duration-300 hover:scale-125`}>News</a></div>
+		</div>
 	)
 }
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,76 +1,104 @@
-import { motion, useInView } from "framer-motion"
+import { motion, useInView, useScroll, useTransform } from "framer-motion"
 import { useRef } from "react"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 
 export default function HomePage() {
-  // Refs to track when sections are in view
-  const aboutRef = useRef(null)
-  const aboutInView = useInView(aboutRef, { once: true, margin: "-50px" })
+	
+	const { scrollYProgress } = useScroll();
+	const x = useTransform(scrollYProgress, [0.2, 0.4], [300, 0])
+	const opacity = useTransform(scrollYProgress, [0.1, 0.35], [0, 1]);
 
-  const slideRef = useRef(null)
-  const slideInView = useInView(slideRef, { once: true, margin: "-50px" })
+	// helper for transitions on smaller viewports
+	const isSmallScreen = window.innerWidth < 768
+
+	const aboutRef = useRef<HTMLElement | null>(null)
+	const aboutInView = useInView(aboutRef, {
+		once: false,
+		margin: isSmallScreen ? "-50px" : "-300px"
+	})
+
+	const slideRef = useRef<HTMLElement | null>(null)
+	const slideInView = useInView(slideRef, { 
+		once: false, 
+		margin: isSmallScreen ? "-50px" : "-200px" 
+	})
+
+	const newsRef = useRef<HTMLDivElement | null>(null)
+
+	// current? calls if exists
+	const scrollToAbout = () => {
+		aboutRef.current?.scrollIntoView({ behavior: "smooth"})
+	}
+
+	const scrollToNews = () => {
+		newsRef.current?.scrollIntoView({ behavior: "smooth"})
+	}
 
   return (
     <div className="bg-zinc-900 text-white flex flex-col min-h-screen">
-      {/* HERO / HEADER SECTION */}
-      <section
-        className="relative flex justify-center h-screen bg-purple-500 bg-cover bg-left"
-        style={{ backgroundImage: "url(/moreTest.jpg)" }}
-      >
-        <Header />
-      </section>
+		{/* Intro w/ header */}
+		<section
+		className="relative flex justify-center h-screen bg-purple-500 bg-cover bg-left"
+		style={{ backgroundImage: "url(/moreTest.jpg)" }}
+		>
+			<Header onScrollToAbout={scrollToAbout} onScrollToNews={scrollToNews}/>
+		</section>
 
-      {/* ABOUT SECTION */}
-      <section
-        ref={aboutRef}
-        className="relative flex items-center justify-center min-h-screen bg-purple-700 bg-cover bg-center"
-        style={{ backgroundImage: "url(/test.jpg)" }}
-      >
-        <motion.div
-          initial={{ opacity: 0, y: 50 }}
-          animate={aboutInView ? { opacity: 1, y: 0 } : {}}
-          transition={{ duration: 0.8 }}
-          className="max-w-3xl p-8 text-center bg-green-400 rounded-lg bg-opacity-80"
-        >
-          <h2 className="text-4xl mb-4">Licht</h2>
-          <p>
-            During an archeological excavation – a set of Gates were discovered.
-            People quickly realized that these ancient passages lead to different
-            realms and dimensions. Rynoka, a small commercial village, was found
-            near the excavation site – providing brave and reckless adventurers
-            with treasures beyond measure. Moonlighter is an Action RPG with
-            rogue-lite elements that demonstrates two sides of the coin – revealing
-            everyday routines of Will, an adventurous shopkeeper that secretly
-            dreams of becoming a hero.
-          </p>
-        </motion.div>
-      </section>
+		{/* About */}
+		<section
+			ref={aboutRef}
+			className="relative flex flex-row items-center justify-center min-h-1500px bg-purple-700 bg-cover bg-center"
+			style={{ backgroundImage: "url(/test.jpg)" }}
+		>
+			<motion.div
+				initial={{ opacity: 0, y: 50 }}
+				animate={aboutInView ? { opacity: 1, y: 0 } : {}}
+				transition={{ duration: 1.5 }}
+				className="flex flex-col flex-1 max-w-3xl p-8 text-center bg-green-400 rounded-lg bg-opacity-80 my-[10%] mx-[2%] ml-[4%]"
+			>
+				<h2 className="text-[clamp(2rem,2vw,4rem)] mb-4">Licht</h2>
+				<p className="text-[clamp(1rem,1vw,3rem)]">
+					During an archeological excavation – a set of Gates were discovered.
+					People quickly realized that these ancient passages lead to different
+					realms and dimensions. Rynoka, a small commercial village, was found
+					near the excavation site – providing brave and reckless adventurers
+					with treasures beyond measure. Moonlighter is an Action RPG with
+					rogue-lite elements that demonstrates two sides of the coin – revealing
+					everyday routines of Will, an adventurous shopkeeper that secretly
+					dreams of becoming a hero.
+				</p>
+			</motion.div>
+			<motion.div style={{ x, opacity }} className="flex flex-1 bg-red-400 mx-[2%] mr-[4%]">
+				<img src='/moreTest.jpg' />
+			</motion.div>
+		</section>
 
-      {/* SLIDESHOW SECTION */}
-      <section
-        ref={slideRef}
-        className="relative flex items-center justify-center min-h-screen bg-blue-400 bg-cover bg-left"
-        style={{ backgroundImage: "url(/moreTest.jpg)" }}
-      >
-        <motion.div
-          initial={{ opacity: 0, y: 50 }}
-          animate={slideInView ? { opacity: 1, y: 0 } : {}}
-          transition={{ duration: 0.8 }}
-          className="max-w-3xl p-8 text-center bg-black bg-opacity-50 rounded-lg"
-        >
-          <h2 className="text-4xl mb-4">Slideshow</h2>
-          <p>
-            This is another section. It will fade in and slide up when scrolled
-            into view.
-          </p>
-        </motion.div>
-      </section>
+		{/* Slideshow */}
+		<section
+			ref={slideRef}
+			className="relative flex items-center justify-center min-h-screen bg-blue-400 bg-cover bg-left"
+			style={{ backgroundImage: "url(/moreTest.jpg)" }}
+		>
+			<motion.div
+				ref={newsRef}
+				initial={{ opacity: 0, y: 50 }}
+				animate={slideInView ? { opacity: 1, y: 0 } : {}}
+				transition={{ duration: 1.5 }}
+				className="max-w-3xl p-8 text-center bg-black bg-opacity-50 rounded-lg"
+			>
+				<h2 className="text-4xl mb-4">Slideshow</h2>
+				<p>
+					This is another section. It will fade in and slide up when scrolled
+					into view.
+				</p>
+			</motion.div>
+		</section>
 
-      {/* FOOTER */}
-      <footer className="bg-purple-950 py-6 text-center">
-        <Footer />
-      </footer>
+		{/* Footer */}
+		<footer className="bg-purple-950 py-6 text-center">
+			<Footer />
+		</footer>
     </div>
   )
 }
